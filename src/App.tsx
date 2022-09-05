6c6673c2cfd1acbd7e8f3ae36d670b5b58f72053
Ravinda Senarath <ravinda.senarath@cognite.com>
2022-09-06 00:14:31 +0530

    Different add methods mutate or amend the array

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useMemo, useState } from 'react';
 import './App.css';
+import styled from 'styled-components';
 
-function App() {
+type User = {
+  firstname: string;
+  lastName: string;
+  age: number;
+}
+
+const UserRow = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+
+const UserTile = styled.div`
+  padding: 5px;
+`
+
+const ButtonRow = styled.div`
+  display:flex;
+  align-items: center;
+`
+
+const App = () => {
+
+  const [users, setUsers] = useState<Array<User>>([{firstname: 'John', lastName: 'Doe', age: 24}]);
+
+  const handleButtonClickNewArray = () => {
+    setUsers([...users, {firstname: 'John ' , lastName: 'Doe ' + users.length, age: 4}]);
+  }
+  const handleButtonClickMutateArray = () => {
+    setUsers(users => {
+      users.push({firstname: 'John ' , lastName: 'Doe ' + users.length, age: 4})
+      return users;
+    })
+  }
+
+  const userNames = useMemo(() => {
+    return users.map(user => `${user.firstname} ${user.lastName}`)
+  }, [users])
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <UserRow>
+          {
+            userNames.map(userName => (<UserTile>{userName}</UserTile>))
+          }
+        </UserRow>
+        <ButtonRow>
+          <button onClick={handleButtonClickNewArray}>Add User ( New Array )</button>
+        </ButtonRow>
+        <ButtonRow>
+          <button onClick={handleButtonClickMutateArray}>Add User ( Mutate Array )</button>
+        </ButtonRow>
       </header>
     </div>
   );
